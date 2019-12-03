b7f13afb888d997abe8b0835a732af93e5c192be
BikashKrTanti <bktbest21@gmail.com>
2019-12-04 00:58:38 +0530

    touch the box and the box colour will change

--- app/src/main/java/com/example/boardgame/Part1Activity.kt
+++ app/src/main/java/com/example/boardgame/Part1Activity.kt
@@ -14,45 +14,68 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_part1.*
 
 class Part1Activity : AppCompatActivity() {
+    public var posx: Float = 0.0f
+    public var posy: Float = 0.0f
     public var height =0
     public var width=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_part1)
 
-       val displayMetrics = DisplayMetrics()
+        var drawBoardDecide=1
+        val displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
         width=displayMetrics.widthPixels
         height=width
 
-        val text: TextView = findViewById(R.id.textView) as TextView
-        text.setOnClickListener { text.setText("h" + height +"w" + width ) }
+        GamePage.setOnTouchListener { _, event ->
+            posx=event.x
+            posy=(event.y/displayMetrics.heightPixels)*width
 
-        val bitmap: Bitmap = Bitmap.createBitmap(1000,1000,Bitmap.Config.ARGB_8888)
-        val canvas: Canvas =Canvas(bitmap)
+            drawBox(posx,posy)
 
+            if (drawBoardDecide==1){
+                drawBoard(height,width)
+                drawBoardDecide=1
+            }
+            val text: TextView = findViewById(R.id.textView) as TextView
+            text.text = "h" + posy +"w" + posx
+
+            true
+        }
+    }
+
+    private fun drawBoard(height: Int, width: Int) {
+        val bitmap: Bitmap = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888)
+        val canvas =Canvas(bitmap)
         for(i in 0..4) {
             for (j in 0..7) {
-                var shapeDrawable: ShapeDrawable
+                var shapeDrawable = ShapeDrawable(RectShape())
                 var left = (i*width/6)+(width/18)
                 var right = left+ (width/6) - 10
                 var top = (j*height/9)+(height/27)
                 var bottom = top+ (height/9)- 10
-
-                shapeDrawable = ShapeDrawable(RectShape())
                 shapeDrawable.setBounds(left, top, right, bottom)
-                if(i==j) {
-                    shapeDrawable.paint.setColor(Color.parseColor("#EC5247"))
-                }
-                else
-                    shapeDrawable.paint.setColor(Color.parseColor("#ffffff"))
-
+                shapeDrawable.paint.color = Color.parseColor("#ffffff")
                 shapeDrawable.draw(canvas)
-                imageView.background = BitmapDrawable(getResources(), bitmap)
+                imageView.background = BitmapDrawable(resources, bitmap)
+            } } }
 
-            }
-
+    private fun drawBox(posnx: Float, posny: Float) {
+        val bitmap: Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        var i=((posnx+(width/9))/(width/6)).toInt()-1
+        var j=((posny+(height/18))/(height/9)).toInt()-1
+        if(i in 0..4 && j in 0..7) {
+            var shapeDrawable = ShapeDrawable(RectShape())
+            var left = (i * width / 6) + (width / 18)
+            var right = left + (width / 6) - 10
+            var top = (j * height / 9) + (height / 27)
+            var bottom = top + (height / 9) - 10
+            shapeDrawable.setBounds(left, top, right, bottom)
+            shapeDrawable.paint.color = Color.parseColor("#EC5247")
+            shapeDrawable.draw(canvas)
+            imageViewBox.background = BitmapDrawable(resources, bitmap)
         }
-
     }
 }
---
+++ app/src/main/java/com/example/boardgame/PowersAssign.kt
@@ -0,0 +1,12 @@
+package com.example.boardgame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PowersAssign : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_powers_assign)
+    }
+}
